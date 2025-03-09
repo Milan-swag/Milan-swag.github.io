f5f044341b8d8fc13017910b583229ac698ed88c
Milan-swag <milan.duettra@gmail.com>
2025-03-09 17:49:06 +0100

    new file:   Epische_Kiste.png new file:   Epische_Kiste_Kaufen.html new file:   Legendaere_Kiste_Kaufen.html new file:   MONetwork.html new file:   Seltene_Kiste_Kaufen.html new file:   Shop.html new file:   Ultra_Kiste_kaufen.html new file:   assets/basic/script.js new file:   assets/basic/style.css new file:   assets/images/Epische_Kiste.png new file:   assets/images/logos/sitelogo.png new file:   assets/pages/mainpage/script.js new file:   assets/pages/mainpage/style.css new file:   assets/pages/style.css new file:   epic-chest/epic-chest-checkout.html new file:   epic-chest/epic-chest.html new file:   legendary-chest/legendary-chest-checkout.html new file:   legendary-chest/legendary-chest.html new file:   rare-chest/rare-chest-checkout.html new file:   rare-chest/rare-chest.html new file:   style.css.css new file:   styleshop.css new file:   ultra-chest/ultra-chest-checkout.html new file:   ultra-chest/ultra-chest.html

---
+++ assets/basic/script.js
@@ -0,0 +1,72 @@
+// Navbar
+document.querySelector(".header-left").addEventListener("click", function() {
+    window.open ("/?ref=navbar_text", "_parent")
+});
+
+// Custom CSS styles based on DIV properties
+(function() {
+    function generateStyles() {
+        const elements = document.querySelectorAll('*');
+        const styles = new Set();
+
+        elements.forEach(element => {
+        const classList = element.classList;
+
+        classList.forEach(cls => {
+            if (cls.startsWith('w') || cls.startsWith('h')) {
+            const parts = cls.substring(1).split('-');
+            const value = parts[0];
+            const decimal = parts[1] ? '.' + parts[1] : ''; 
+            const finalValue = value + decimal;
+            const property = cls.charAt(0) === 'w' ? 'width' : 'height';
+
+            if (!isNaN(finalValue)) {
+                styles.add(`.${cls} { ${property}: ${finalValue}px; }`);
+            }
+            }
+        });
+        });
+        return Array.from(styles).join('\n');
+    }
+
+    const styleTag = document.createElement('style');
+    styleTag.type = 'text/css';
+    styleTag.appendChild(document.createTextNode(generateStyles()));
+    document.head.appendChild(styleTag);
+})();
+
+function getDeviceType() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (/mobile|android|iphone|ipad|ipod|blackberry|windows phone/i.test(userAgent)) {
+       return "mobile";
+    } else {
+        return "desktop";
+    }
+}
+
+function getBrowser() {
+    const userAgent = navigator.userAgent;
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "mf_mozilla-firefox";
+    } else if (userAgent.indexOf("SamsungBrowser") > -1) {
+        return "si_samsung-internet";
+    } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+        return "op_opera";
+    } else if (userAgent.indexOf("Trident") > -1) {
+        return "ie_internet-Explorer";
+    } else if (userAgent.indexOf("Edge") > -1) {
+        return "me_microsoft-edge";
+    } else if (userAgent.indexOf("Chrome") > -1) {
+        return "gc_google-chrome";
+    } else if (userAgent.indexOf("Safari") > -1) {
+        return "as_apple-safari";
+    } else {
+        return "uk_unknown";
+    }
+}
+
+const deviceType = getDeviceType();
+const browser = getBrowser();
+
+console.log(`set-userdevice=${deviceType}`);
+console.log(`set-userbrowser=${browser}`);
---
+++ assets/pages/mainpage/script.js
@@ -0,0 +1,15 @@
+document.getElementById("epic-chest").addEventListener("click", function() {
+    window.open("epic-chest/epic-chest.html?purchase-type=epic-chest&utm_src=mainpage_purchase-chest_area", "_blank");
+});
+
+document.getElementById("rare-chest").addEventListener("click", function() {
+    window.open("rare-chest/rare-chest.html?purchase-type=rare-chest&utm_src=mainpage_purchase-chest_area", "_blank");
+});
+
+document.getElementById("legendary-chest").addEventListener("click", function() {
+    window.open("legendary-chest/legendary-chest.html?purchase-type=legendary-chest&utm_src=mainpage_purchase-chest_area", "_blank");
+});
+
+document.getElementById("ultra-chest").addEventListener("click", function() {
+    window.open("ultra-chest/ultra-chest.html?purchase-type=ultra-chest&utm_src=mainpage_purchase-chest_area", "_blank");
+});
